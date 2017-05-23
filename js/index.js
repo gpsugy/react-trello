650896d3b02a40c67c7030ebcaffa6208b137004
Robby Kim <robby@thenewfoundry.com>
2017-05-23 12:13:12 -0400

    Change index.html to use ES6 JSX element notation

--- js/index.js
+++ js/index.js
@@ -7,7 +7,11 @@ var Board = require('./components/board');
 
 var listTitle = ["Uzumaki Naruto", "Uchiha Sasuke", "Haruno Sakura"];
 
-document.addEventListener('DOMContentLoaded', function() {
-	ReactDOM.render(
-		<Board title="Shinobi Directory" lists={listTitle}/>, document.getElementById('app'));
-});
+const element = (
+	<Board title="Shinobi Directory" lists={listTitle}/>
+);
+
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);
