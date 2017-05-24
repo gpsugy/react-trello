252545a232f91e1e9b26026e74fd6d0374866d6d
Robby Kim <robby@thenewfoundry.com>
2017-05-24 12:34:33 -0400

    Add working inputs for each list

--- js/components/list.js
+++ js/components/list.js
@@ -3,13 +3,39 @@ var ReactDOM = require('react-dom');
 
 var Card = require('./card');
 
-function List(props) {
-	return (
-		<div>
-			<div>List Title: {props.title}</div>
-			<Card cards={props.cards} />
-		</div>
-	);
+export class List extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: ''};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+
+		console.log(`Input changed to: ${event.target.value}`);
+	}
+
+	handleSubmit(event) {
+		console.log(`Input ${this.state.value} has been submitted.`);
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<div>
+				<div>List Title: {this.props.title}</div>
+				<Card cards={this.props.cards} />
+				<form onSubmit={this.handleSubmit}>
+					<input type="text" value={this.state.value} onChange={this.handleChange} />
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);	
+	}
 };
 
 module.exports = List;
+
